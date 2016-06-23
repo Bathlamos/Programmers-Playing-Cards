1756db8a9057bb10b16ba59df225f2c6c036fff7
Philippe Legault <plegault@live.ca>
2016-06-23 17:44:41 -0400

    Update 4 - R.r

--- ♦ Diamond/4 - R.r
+++ ♦ Diamond/4 - R.r
@@ -1,5 +1,8 @@
-deck <- data.frame(card = 1:52,
-    suit = suits(), rank = ranks())
+deck <- data.frame(
+          card = 1:52,
+          suit = suits(),       
+          rank = ranks())
 Four.of.Diamond <-
-    deck$card[deck$suit=="diamond"
+  deck$card[
+    deck$suit=="diamond"
     & deck$rank=="4"]
