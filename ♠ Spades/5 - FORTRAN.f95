6fe25f62f6346f083469da2be31a467956438a85
Philippe Legault <plegault@live.ca>
2016-06-23 21:29:38 -0400

    Update 5 - FORTRAN.f95

--- ♠ Spades/5 - FORTRAN.f95
+++ ♠ Spades/5 - FORTRAN.f95
@@ -1,9 +1,8 @@
 TYPE :: Card
-	integer		    :: Rank
-	character(20)	:: Suit
+  integer       :: Rank
+  character(20) :: Suit
 END TYPE Card
 
 TYPE(Card) :: Five
-
 Five%Rank = 5
 Five%Suit = 'spades'
