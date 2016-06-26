af17c4e145dc6210701960e8ef31323cb0fdd288
Philippe Legault <plegault@live.ca>
2016-06-25 22:15:38 -0400

    Added indentation

--- ♠ Spades/12 - Bash.sh
+++ ♠ Spades/12 - Bash.sh
@@ -1,1 +1,5 @@
-rm Spades;  for i in {1..12}; do echo $i >> Spades; done; wc -l Spades | sed 's/12/Queen/'
+rm Spades;  
+for i in {1..12}; 
+  do echo $i >> Spades;  
+done; 
+wc -l Spades | sed 's/12/Queen/'
