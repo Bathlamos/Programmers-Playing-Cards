2c6e8081a0c96e3b840e82fc69305aec63b31535
Philippe Legault <plegault@live.ca>
2016-06-26 08:47:55 -0400

    Update 11 - C#.cs

--- ♣ Clubs/11 - C#.cs
+++ ♣ Clubs/11 - C#.cs
@@ -1,8 +1,8 @@
-public static void Main(string[] args){
-     string[] card = new string[3];
-     card[0] = "11"; //rank
-     card[1] = "Jack"; //name
-     card[2] = "clubs"; //suit
+//string[] card;
+card = new string[3];
+card[0] = "11";
+card[1] = "Jack";
+card[2] = "clubs";
 
-     foreach (string s in card) { Console.WriteLine(s); }
-}
+foreach (string s in card) 
+  Console.WriteLine(s);
